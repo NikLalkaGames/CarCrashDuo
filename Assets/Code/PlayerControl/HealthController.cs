7948659625bcbb2d85a7541cc567d54778a1cb6d
Farhinarius <farhat3027@gmail.com>
2022-11-21 20:48:57 +0300

    add HealthController

---
+++ Assets/Code/PlayerControl/HealthController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.PlayerControl
+{
+    public class HealthController : MonoBehaviour
+    {
+        [SerializeField] private Slider UIHealth;
+
+        [SerializeField] private float _maxHealth;
+
+        private void Start()
+        {
+            UIHealth.maxValue = _maxHealth;
+        }
+
+        public void ChangeHealth(float amountToChange)      // can be negative
+        {
+            UIHealth.value += amountToChange;
+        }
+
+        public void UpdateHealth(float healthValueToUpdate)
+        {
+            UIHealth.value = healthValueToUpdate;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                UpdateHealth(_maxHealth);
+            }
+            
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                ChangeHealth(-30);
+            }
+        }
+    }
+}
