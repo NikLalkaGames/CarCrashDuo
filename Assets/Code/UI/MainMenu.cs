33d4fe48d0e9092781b842f6d9ee16e65d1e5417
Farhinarius <farhat3027@gmail.com>
2022-12-01 23:28:15 +0300

    Fix Lighting scene transition and road spawning

--- Assets/Code/UI/MainMenu.cs
+++ Assets/Code/UI/MainMenu.cs
@@ -6,26 +6,13 @@ namespace Code.UI
     public class MainMenu : MonoBehaviour
     {
         [SerializeField] private string _playButtonScene;
-
         [SerializeField] private string _settingsButtonScene;
-
         [SerializeField] private string _creditsButtonScene;
 
         public void OnPlayButtonClicked()
         {
             SceneManager.LoadScene(_playButtonScene);
         }
-
-        public void OnSettingsButtonClicked()
-        {
-            Debug.Log("Settings button clicked");
-        }
-
-        public void OnCreditsButtonClicked()
-        {
-            Debug.Log("Credits button clicked");
-        }
-
         public void OnExitButtonClicked()
         {
             Application.Quit();
